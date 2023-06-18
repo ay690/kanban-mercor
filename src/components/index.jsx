9993d9d11d61501774740d596f39a61de520ca57
Aniket Yadav <aniketyadav690@gmail.com>
2023-06-18 23:26:36 +0530

    kanban

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import Filter from "./Filter";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import TaskBoard from "./TaskBoard";
+
+export { Filter, Navbar, Sidebar, TaskBoard };
